01f8731a6fb0b8a4ec1fbbf0c3879593fde8be02
Waldemar Głaz <w.glaz@cheil.com>
2022-05-19 00:36:50 +0200

    location body text 375px

--- src/Components/location/LocationTextBody.styled.js
+++ src/Components/location/LocationTextBody.styled.js
@@ -2,9 +2,31 @@ import styled from "styled-components";
 export const StyledLocationTextBody = styled.div`
   background-color: #151515;
   padding: 3rem 1rem;
+  color: #fff;
   & .title {
     color: #fff;
     text-transform: uppercase;
     font-size: 3.125rem;
+    margin: 0 0 3rem;
+  }
+  & h3 {
+    color: #d5966c;
+    text-transform: uppercase;
+    font-size: 2rem;
+    font-weight: 900;
+    margin: 0;
+  }
+  & p {
+    font-family: "Outfit", sans-serif;
+    font-weight: 300;
+    font-size: 1.125rem;
+    font-style: normal;
+    margin: 0;
+    line-height: 28px;
+  }
+  & .text-content {
+    margin-top: 1.25rem;
+    font-family: "Outfit", sans-serif;
+    line-height: 28px;
   }
 `;
